922fc96990a67044ea769425b0aa0a29cf1f031d
MarcelHB <marcel.heing@tu-harburg.de>
2013-04-26 17:48:38 +0200

    FITIn-reg: adds test case for branch switch demo

---
+++ fitin/tests/sample2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#include "../../../valgrind/include/valgrind/fi_client.h"
+
+int main() {
+    int a = 1;
+    FITIN_MONITOR_VARIABLE(a);
+
+    if(a) {
+        printf("a GE 1\n");
+    } else {
+        printf("a LT 1\n");
+    }
+
+    return 0;
+}
