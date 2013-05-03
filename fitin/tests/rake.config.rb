551e77495d324f41f006362b9f37cf339bb5a51e
MarcelHB <marcel.heing@tu-harburg.de>
2013-05-03 23:13:29 +0200

    FITIn-reg: increases benchmark runs to 100

--- fitin/tests/rake.config.rb
+++ fitin/tests/rake.config.rb
@@ -9,5 +9,5 @@ TEST_DATA = "tests.json"
 VALGRIND = "../../bin/valgrind"
 VG_FLAGS = "--tool=fitin"
 
-BENCHMARK_RUNS = 10
+BENCHMARK_RUNS = 100
 BENCHMARK_DHRY_VG = "--mod-bit=3 --mod-load-time=10"
