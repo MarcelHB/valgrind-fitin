190db084224aa345856753aaecc199d190ba45e4
MarcelHB <marcel.heing@tuhh.de>
2014-04-09 14:06:18 +0200

    FITIn: completes Fortran support

---
+++ fitin/fortran/fi_client.c
@@ -0,0 +1,9 @@
+#include <fi_client.h> 
+
+void fitin_monitor_memory_(void **ptr, unsigned int size) {
+    FITIN_MONITOR_MEMORY(*ptr, size);
+}
+
+void fitin_unmonitor_memory_(void **ptr, unsigned int size) {
+    FITIN_UNMONITOR_MEMORY(*ptr, size);
+}
--- fitin/tests/fortran/fi_client.c
+++
@@ -1,5 +0,0 @@
-#include "../../../include/valgrind/fi_client.h"
-
-void fitin_monitor_memory_(void **ptr, unsigned int size) {
-    FITIN_MONITOR_MEMORY(*ptr, size);
-}
